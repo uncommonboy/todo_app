1e51ca0cd3acf336c8a299a66e1955b796700f77
uncommonboy <abdugafforovasilbek206@gmail.com>
2023-04-19 21:36:34 +0500

    salom

---
+++ script.js
@@ -0,0 +1,33 @@
+const form = document.querySelector(".form");
+const list = document.querySelector(".todosList");
+const clear = document.querySelector(".clear");
+console.log(form);
+const todos = [];
+
+const render = () => {
+  list.innerHTML = "";
+  for (let element of todos) {
+    list.innerHTML += ` <li class="todo">
+        <input value="${element.value}" class="todo_input" type="text" />
+        <div class="edit">
+          <i class="bx bx-sm bxs-pencil"></i>
+        </div>
+        <div class="delete">
+          <i class="bx bx-sm bx-trash"></i>
+        </div>
+      </li>`;
+  }
+};
+render();
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const inputValue = event.target[0].value;
+  const newTodo = { value: inputValue, id: Date.now() + "#" };
+  todos.unshift(newTodo);
+  render();
+});
+
+clear.addEventListener("click", () => {
+  list.innerHTML=null;
+});
