b1c9039fedf55e07ad439f9e0eee7056ece0cc42
uncommonboy <abdugafforovasilbek206@gmail.com>
2023-04-24 14:20:34 +0500

    totally fixed

--- script.js
+++ script.js
@@ -3,13 +3,20 @@ const list = document.querySelector(".todosList");
 const clear = document.querySelector(".clear");
 const add = document.querySelector(".add");
 const edit = document.querySelector("#edit");
-let todos = [{value:`reading books ${checkbox=true  }`, value:'playing football'}];
+let todos = [
+  { value: `reading books`, isdone: true ,id:1},
+  { value: "playing football", isdone: false,id:2 },
+];
 
 const render = () => {
   list.innerHTML = "";
   for (let element of todos) {
+    const checkbox = element.isdone;
+    console.log(checkbox);
     list.innerHTML += ` <li class="todo">
-    <input class='checkbox'  ${checkbox == true ? "checked" : ""} onclick="oncheck('${element.id}')" type="checkbox"/> 
+    <input class='checkbox'  ${
+      checkbox == true ? "checked" : ""
+    } onclick="oncheck('${element.id}')" type="checkbox"/> 
 
       <input disabled="disabled" value="${
         element.value
@@ -29,8 +36,9 @@ const deleteTodo = (id) => {
   render();
   console.log("delete", id, todos);
 };
+
 const oncheck = (id) => {
-  todos = todos.map((v) => (v.id == id ? { ...v, isdone: !isdone } : v));
+  todos = todos.map((v) => (v.id == id ? { ...v, isdone: !v.isdone } : v));
   render();
   console.log("check", todos);
 };
@@ -38,6 +46,11 @@ const oncheck = (id) => {
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   const inputValue = event.target[0].value;
+  event.target[0].value = ""
+    if(inputValue == ""){
+        alert("enter any note")
+        return
+    }
   const newTodo = { value: inputValue, id: Date.now() + "#" };
   todos.unshift(newTodo);
   render();
